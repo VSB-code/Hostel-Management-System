dd9a6c7d1a7cf6b1b2560e390cd1961688f5d804
VIRENDRA SINGH BHATI <virendrasinghbhati2008@gmail.com>
2026-06-16 09:48:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="",      
+        password="...",      
         database="hostel_management"       
     )
 

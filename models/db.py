1a6a47cae2868250c58164b25e78ede114c11a67
VSB-code <virendrasinghbhati2008@gmail.com>
2026-07-22 22:28:42 +0530

    Deployment ready

--- models/db.py
+++ models/db.py
@@ -2,6 +2,8 @@ import os
 import mysql.connector
 from config import Config
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CA_PATH = os.path.join(BASE_DIR, "certs", "ca.pem")
 
 def get_db_connection():
     return mysql.connector.connect(
@@ -10,6 +12,6 @@ def get_db_connection():
         user=Config.DB_USER,
         password=Config.DB_PASSWORD,
         database=Config.DB_NAME,
-        ssl_ca=r"C:\Users\VIRENDRASINGH\Downloads\ca.pem",
-        use_pure=True  
+        ssl_ca=CA_PATH,
+        use_pure=True
     )
